56794e38eb285d012465a547a9c23aa82918b906
Mary Ellen Bowman <maryellen.bowman@gmail.com>
2024-03-11 00:55:56 +0000

    model classes

---
+++ explorecali-jpa/src/main/java/com/example/explorecalijpa/model/Difficulty.java
@@ -0,0 +1,14 @@
+package com.example.explorecalijpa.model;
+
+/**
+ * Created by maryellenbowman on 7/3/16.
+ */
+
+/**
+ * Enumeration of the level of effort.
+ *
+ * Created by Mary Ellen Bowman
+ */
+public enum Difficulty {
+    Easy, Medium, Difficult, Varies;
+}
---
+++ explorecali-jpa/src/main/java/com/example/explorecalijpa/model/TourPackage.java
@@ -0,0 +1,60 @@
+package com.example.explorecalijpa.model;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.util.Objects;
+
+/**
+ * A Classification of Tours.
+ *
+ * Created by Mary Ellen Bowman
+ */
+@Table(name="tour_package")
+@Entity
+public class TourPackage {
+    @Id
+    private String code;
+
+    @Column
+    private String name;
+
+    protected TourPackage() {
+    }
+
+    public TourPackage(String code, String name) {
+        this.code = code;
+        this.name = name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "TourPackage{" +
+                "code='" + code + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        TourPackage that = (TourPackage) o;
+        return Objects.equals(code, that.code) &&
+                Objects.equals(name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(code, name);
+    }
+}

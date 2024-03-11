4eab3edec1808c8555fa62be8fb99f6dbe66d084
Mary Ellen Bowman <maryellen.bowman@gmail.com>
2024-03-11 00:16:22 +0000

    spring initializer

---
+++ explorecali-jpa/src/main/java/com/example/explorecalijpa/ExplorecaliJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.explorecalijpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExplorecaliJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExplorecaliJpaApplication.class, args);
+	}
+
+}
---
+++ explorecali-jpa/src/test/java/com/example/explorecalijpa/ExplorecaliJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.explorecalijpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExplorecaliJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

7f2d5647bddadd4ba905c0ad21d646ea27bc9e7d
Mary Ellen Bowman <maryellen.bowman@gmail.com>
2024-03-16 19:56:16 +0000

    before challenge

--- explorecali-jpa/src/main/java/com/example/explorecalijpa/repo/TourRepository.java
+++ explorecali-jpa/src/main/java/com/example/explorecalijpa/repo/TourRepository.java
@@ -1,13 +1,8 @@
 package com.example.explorecalijpa.repo;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.example.explorecalijpa.model.Difficulty;
 import com.example.explorecalijpa.model.Tour;
 
 public interface TourRepository extends JpaRepository<Tour, Integer> {
-  List<Tour> findByDifficulty(Difficulty diff);
-  List<Tour> findByTourPackageCode(String code);
 }

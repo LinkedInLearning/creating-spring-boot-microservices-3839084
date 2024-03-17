1cc00ae43492e51d253939c13e6d5485f8a99030
Mary Ellen Bowman <maryellen.bowman@gmail.com>
2024-03-17 19:55:57 +0000

    challenge answer

--- explorecali-jpa/src/main/java/com/example/explorecalijpa/business/TourService.java
+++ explorecali-jpa/src/main/java/com/example/explorecalijpa/business/TourService.java
@@ -33,11 +33,11 @@ public class TourService {
   }
 
   public List<Tour> lookupByDifficulty(Difficulty difficulty) {
-    return Collections.emptyList();
+    return tourRepository.findByDifficulty(difficulty);
   }
 
   public List<Tour> lookupByPackage(String tourPackageCode) {
-    return Collections.emptyList();
+    return tourRepository.findByTourPackageCode(tourPackageCode);
   }
 
   public long total() {

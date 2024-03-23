41507ac214ae6c7ef3e8f17365b47ca4b9093c78
Mary Ellen Bowman <maryellen.bowman@gmail.com>
2024-03-23 17:40:47 +0000

    valid

--- explorecali-jpa/src/main/java/com/example/explorecalijpa/web/TourRatingController.java
+++ explorecali-jpa/src/main/java/com/example/explorecalijpa/web/TourRatingController.java
@@ -1,12 +1,10 @@
 package com.example.explorecalijpa.web;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.NoSuchElementException;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,6 +18,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.explorecalijpa.business.TourRatingService;
 import com.example.explorecalijpa.model.TourRating;
+
+import jakarta.validation.Valid;
 
 /**
  * Tour Rating Controller
@@ -44,7 +44,7 @@ public class TourRatingController {
   @PostMapping
   @ResponseStatus(HttpStatus.CREATED)
   public RatingDto createTourRating(@PathVariable(value = "tourId") int tourId,
-      @RequestBody @Validated RatingDto ratingDto) {
+      @RequestBody @Valid RatingDto ratingDto) {
       TourRating rating = tourRatingService.createNew(tourId, ratingDto.getCustomerId(), 
         ratingDto.getScore(), ratingDto.getComment());
       return new RatingDto(rating);
@@ -75,7 +75,7 @@ public class TourRatingController {
    * @return The modified Rating DTO.
    */
   @PutMapping
-  public RatingDto updateWithPut(@PathVariable(value = "tourId") int tourId, @RequestBody @Validated RatingDto ratingDto) {
+  public RatingDto updateWithPut(@PathVariable(value = "tourId") int tourId, @RequestBody @Valid RatingDto ratingDto) {
       return new RatingDto(tourRatingService.update(tourId, ratingDto.getCustomerId(),
                 ratingDto.getScore(), ratingDto.getComment()));
   }

8f502e3f7f9e9a9971a37fcf5d5f74a34fe11db0
Mary Ellen Bowman <maryellen.bowman@gmail.com>
2024-03-23 02:33:10 +0000

    Http Post

--- explorecali-jpa/src/main/java/com/example/explorecalijpa/web/TourRatingController.java
+++ explorecali-jpa/src/main/java/com/example/explorecalijpa/web/TourRatingController.java
@@ -1,6 +1,15 @@
 package com.example.explorecalijpa.web;
 
+import java.util.NoSuchElementException;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.explorecalijpa.business.TourRatingService;
@@ -18,5 +27,24 @@ public class TourRatingController {
   public TourRatingController(TourRatingService tourRatingService) {
     this.tourRatingService = tourRatingService;
   }
-  
+
+  /**
+   * Create a Tour Rating.
+   *
+   * @param tourId
+   * @param ratingDto
+   */
+  @PostMapping
+  @ResponseStatus(HttpStatus.CREATED)
+  public void createTourRating(@PathVariable(value = "tourId") int tourId,
+      @RequestBody @Validated RatingDto ratingDto) {
+    tourRatingService.createNew(tourId, ratingDto.getCustomerId(), ratingDto.getScore(), ratingDto.getComment());
+  }
+
+  @ExceptionHandler(NoSuchElementException.class)
+  @ResponseStatus(HttpStatus.NOT_FOUND)
+  public String return404(NoSuchElementException exception) {
+    return exception.getMessage();
+  }
+
 }

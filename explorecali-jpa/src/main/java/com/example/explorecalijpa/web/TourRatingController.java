e3beb8ed7b63501be6ae97a28296e15111b92958
Mary Ellen Bowman <maryellen.bowman@gmail.com>
2024-03-23 13:03:11 +0000

    add annotation

--- explorecali-jpa/src/main/java/com/example/explorecalijpa/web/TourRatingController.java
+++ explorecali-jpa/src/main/java/com/example/explorecalijpa/web/TourRatingController.java
@@ -19,6 +19,8 @@ import com.example.explorecalijpa.business.TourRatingService;
  *
  * Created by Mary Ellen Bowman
  */
+@RestController
+@RequestMapping(path = "/tours/{tourId}/ratings")
 public class TourRatingController {
   private TourRatingService tourRatingService;
 

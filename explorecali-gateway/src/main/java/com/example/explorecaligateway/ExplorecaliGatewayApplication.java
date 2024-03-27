7e5fad85635716730026b356255ff133486f3d32
Mary Ellen Bowman <maryellen.bowman@gmail.com>
2024-03-27 22:48:48 +0000

    first contents

---
+++ explorecali-gateway/src/main/java/com/example/explorecaligateway/ExplorecaliGatewayApplication.java
@@ -0,0 +1,14 @@
+package com.example.explorecaligateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@SpringBootApplication
+public class ExplorecaliGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExplorecaliGatewayApplication.class, args);
+	}
+
+}

5e34067e51d76eb407e5c729919099517610942d
Mary Ellen Bowman <maryellen.bowman@gmail.com>
2024-03-27 22:06:15 +0000

    add logging

--- explorecali-images/src/main/java/com/example/explorecaliimages/web/ImageController.java
+++ explorecali-images/src/main/java/com/example/explorecaliimages/web/ImageController.java
@@ -36,7 +36,7 @@ public class ImageController {
     @Operation(summary = "Upload a file to add to the online brochure")
     public String handleFileUpload(@RequestParam("file") MultipartFile file) throws IOException {
 
-        System.out.println(file.getOriginalFilename());
+        log.info("POST /api/images {}", file.getOriginalFilename());
         Image image = new Image();
         image.setFileName(file.getOriginalFilename());
         image.setData(file.getBytes());
@@ -47,6 +47,7 @@ public class ImageController {
     @GetMapping("/{id}")
     @Operation(summary = "Fetch the file by mongo identifier")
     public ResponseEntity<byte[]> handleFileDownload(@PathVariable String id) {
+        log.info("GET /api/images/{}", id);
         return service.getImage(id).map(i ->  ResponseEntity.ok().header("Content-Disposition", "attachment; filename=\"" + i.getFileName() + "\"")
                     .body(i.getData())).orElse(ResponseEntity.notFound().build());
     }
@@ -54,6 +55,7 @@ public class ImageController {
     @GetMapping
     @Operation(summary = "List all filenames with their mongodb identifier")
     public List<IdName> findAll() {
+        log.info("GET /api/images");
         return service.findIdNames();
     }
 }

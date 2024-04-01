78a97af410dadbc79b572b91bbce09ad059810f7
Mary Ellen Bowman <maryellen.bowman@gmail.com>
2024-04-01 02:33:35 +0000

    spring beans

--- tour-service/src/main/java/com/example/Application.java
+++ tour-service/src/main/java/com/example/Application.java
@@ -1,13 +1,17 @@
 package com.example;
-import com.example.tourservice.utilities.TourInitializer;
-import com.example.tourservice.utilities.TourRepository;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.example.tourservice.SpringBeans;
 import com.example.tourservice.utilities.TravelAgentService;
 
 public class Application {
+
     public static void main(String[] args) {
-        TourRepository tr = new TourRepository();
-        TourInitializer as = new TourInitializer(tr);
-        TravelAgentService agentService = new TravelAgentService(tr);
-        agentService.display();
+        ApplicationContext context = new AnnotationConfigApplicationContext(SpringBeans.class);
+        TravelAgentService agentService = context.getBean(TravelAgentService.class);
+
+        agentService.displayTours();
     }
 }
---
+++ tour-service/src/main/java/com/example/tourservice/SpringBeans.java
@@ -0,0 +1,26 @@
+package com.example.tourservice;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.example.tourservice.utilities.TourInitializer;
+import com.example.tourservice.utilities.TourRepository;
+import com.example.tourservice.utilities.TravelAgentService;
+
+@Configuration
+public class SpringBeans {
+ 
+  @Bean
+  public TourRepository tourRepository() {
+    return new TourRepository();
+  }
+  @Bean
+  public TourInitializer tourInitializer() {
+    return new TourInitializer(tourRepository());
+  }
+
+  @Bean
+  public TravelAgentService travelAgentService() {
+    return new TravelAgentService(tourRepository());
+  } 
+}

5d1c6c9ccb35150783f52845e0d436277ca328ce
Mary Ellen Bowman <maryellen.bowman@gmail.com>
2024-04-05 12:19:54 +0000

    remove unnneeded stuff

--- explorecali-jpa/src/main/java/com/example/explorecalijpa/repo/TourPackageRepository.java
+++ explorecali-jpa/src/main/java/com/example/explorecalijpa/repo/TourPackageRepository.java
@@ -5,13 +5,10 @@ import com.example.explorecalijpa.model.TourPackage;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 /*
  * Challenge: Change url keyword to "packages"
  */
-
-@RepositoryRestResource(path="packages", collectionResourceRel = "packages")
 public interface TourPackageRepository extends JpaRepository<TourPackage, String> {
   Optional<TourPackage> findByName(String name);
 }

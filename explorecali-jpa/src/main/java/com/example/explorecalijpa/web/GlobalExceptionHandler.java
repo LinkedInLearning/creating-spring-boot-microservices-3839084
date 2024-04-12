69cd303f9b9ca3459eed278415a110e03d6e03c1
Mary Ellen Bowman <maryellen.bowman@gmail.com>
2024-04-11 22:25:44 -0400

    Update GlobalExceptionHandler.java
    resource exception

--- explorecali-jpa/src/main/java/com/example/explorecalijpa/web/GlobalExceptionHandler.java
+++ explorecali-jpa/src/main/java/com/example/explorecalijpa/web/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.example.explorecalijpa.web;
 
 import java.util.NoSuchElementException;
 
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +15,21 @@ import jakarta.validation.ConstraintViolationException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+  
+  /**
+   * Leverage Exception Handler framework for resource not found Exception.
+   * 
+   * @param ex      ResourceNotFoundException
+   * @param request WebRequest
+   * @return http response
+   */
+  @ExceptionHandler(ResourceNotFoundException.class)
+  public final ResponseEntity<Object> handleResourceNotFoundException(
+      ResourceNotFoundException ex, WebRequest request) {
+    
+    ProblemDetail pd = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, ex.getMessage());
+    return  createResponseEntity(pd, null, HttpStatus.NOT_FOUND, request);
+  }
   
   /**
    * Leverage Exception Handler frameworf for id not found Exception.

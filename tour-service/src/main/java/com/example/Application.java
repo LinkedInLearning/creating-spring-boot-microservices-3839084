bf317b1c8ce0367698f7b025eda4767999a466c3
Mary Ellen Bowman <maryellen.bowman@gmail.com>
2024-04-12 16:30:24 +0000

    Challenge solution

--- tour-service/src/main/java/com/example/Application.java
+++ tour-service/src/main/java/com/example/Application.java
@@ -3,6 +3,7 @@ package com.example;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import com.example.tourservice.TourManagementService;
 import com.example.tourservice.TravelAgentService;
 
 public class Application {
@@ -11,6 +12,8 @@ public class Application {
 
         ApplicationContext context = new AnnotationConfigApplicationContext(Application.class.getPackageName());
         TravelAgentService agent = context.getBean(TravelAgentService.class);
+        TourManagementService manager = context.getBean(TourManagementService.class);
+        manager.createTour("Zoo Tour", 100, true);
         System.out.println("\n******Explore California Tour Catalogue******");
         agent.displayTours();
 
